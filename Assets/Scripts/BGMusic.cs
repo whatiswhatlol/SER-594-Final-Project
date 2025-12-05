f1f092d6cff3e59e4042d999d44f457243161908
Jonathan Lin <jonathanylin1111@gmail.com>
2025-12-05 04:43:21 -0700

    Fix background music repeating itself when going back to main menu

--- Assets/Scripts/BGMusic.cs
+++ Assets/Scripts/BGMusic.cs
@@ -4,6 +4,11 @@ public class BGMusic : MonoBehaviour
 {
     void Start()
     {
+        GameObject backgroundMusic = GameObject.Find("BackgroundMusic");
+        if (backgroundMusic != gameObject) {
+            Destroy(gameObject);
+        }
+
         DontDestroyOnLoad(transform.gameObject);
     }
 }
